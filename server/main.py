615076dd02c081ae9bc7c77c193ab3d85cbc1c58
ShivamTripathi028 <shivamamartripathi2004@gmail.com>
2025-09-10 16:52:27 +0530

    update backend cors list

--- server/main.py
+++ server/main.py
@@ -15,6 +15,7 @@ app = FastAPI(
 origins = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "https://rak-knowledge-hub.vercel.app",
 ]
 
 FRONTEND_URL = os.environ.get("FRONTEND_URL")
