bc8975d46ad491ae8db7ed2426bcf880867480ac
Arjun Dangle <arjun,dangle03@gmail.com>
2025-10-29 20:23:20 +0530

    Fixed build.sh

--- server/build.sh
+++ server/build.sh
@@ -1,23 +1,29 @@
-    #!/usr/bin/env bash
-    set -o errexit
+#!/usr/bin/env bash
+set -o errexit
 
-    echo "--- Starting build process ---"
+echo "--- Starting build process ---"
 
-    echo "--- Installing Python dependencies ---"
-    pip install -r requirements.txt
+echo "--- Installing Python dependencies ---"
+pip install -r requirements.txt
 
-    echo "--- Checking Node.js version ---"
-    node -v
-    npm -v
+echo "--- Checking Node.js version ---"
+node -v
+npm -v
 
-    # Specify the Prisma CLI version required by the Python client
-    PRISMA_CLI_VERSION="5.17.0"
-    echo "--- Using Prisma CLI version $PRISMA_CLI_VERSION ---"
+# Specify the Prisma CLI version required by the Python client
+PRISMA_CLI_VERSION="5.17.0"
+echo "--- Using Prisma CLI version $PRISMA_CLI_VERSION ---"
 
-    echo "--- Generating Prisma Client ---"
-    npx prisma@$PRISMA_CLI_VERSION generate
+# --- Add this line ---
+echo "--- Fetching Prisma Python engine binaries ---"
+python -m prisma fetch
+# --- End of added line ---
 
-    echo "--- Applying database migrations ---"
-    npx prisma@$PRISMA_CLI_VERSION migrate deploy
+echo "--- Generating Prisma Client ---"
+npx prisma@$PRISMA_CLI_VERSION generate
 
-    echo "--- Build finished ---"
+echo "--- Applying database migrations ---"
+npx prisma@$PRISMA_CLI_VERSION migrate deploy
+
+echo "--- Build finished ---"
+
