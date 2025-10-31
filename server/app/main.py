3c33d264d72899eed2ce7342460e1b48d0a5d4e3
ShivamTripathi028 <shivamamartripathi2004@gmail.com>
2025-10-31 19:46:48 +0530

    allow all origins

--- server/app/main.py
+++ server/app/main.py
@@ -68,7 +68,7 @@ if FRONTEND_URL:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
